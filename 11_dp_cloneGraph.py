72e0aff3c6101233eb75fea78f5785d5314d09a7
inu <vr1090@gmail.com>
2022-03-26 10:34:32 +0700

    add clone graph

---
+++ 11_dp_cloneGraph.py
@@ -0,0 +1,21 @@
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        mapper = {}
+        
+        def dfs(n):
+            if n in mapper:
+                return mapper[n]
+            
+            newNode = Node(n.val)
+            mapper[n] = newNode
+            for ne in n.neighbors:
+                newNe = dfs(ne)
+                newNode.neighbors.append(newNe)
+            
+        
+            
+            return newNode
+        
+        dfs(node)
+        
+        return mapper[node]
