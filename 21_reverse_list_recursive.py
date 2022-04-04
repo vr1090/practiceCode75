432dab06a1964f21752bb00a2b4504d1d0cef6a3
inu <vr1090@gmail.com>
2022-04-05 06:54:33 +0700

    update reverse list recusive

--- 21_reverse_list_recursive.py
+++ 21_reverse_list_recursive.py
@@ -14,5 +14,6 @@ class Solution:
         if head.next:
             newHead = self.reverseList(head.next)
             head.next.next = head
+            head.next = None
         
         return newHead
