c8e874b7e4b56a6923aa086b8a69b594e36e59b9
inu <vr1090@gmail.com>
2022-04-14 01:02:41 +0700

    add word search

--- 31_word_search.py
+++ 31_word_search.py
@@ -1,36 +1,38 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        lenRow = len(board)
-        lenCol = len(board[0])
-        
+        lenR = len(board)
+        lenC = len(board[0])
         visited = set()
         
-        def dfs(r,c,i):
-            
+        def dfs(r,c, i):
             if i == len(word):
                 return True
             
-            if r < 0 or c < 0 or r == lenRow or c == lenCol:
+            if r < 0 or c < 0 or r >= lenR or c >= lenC:
                 return False
             
-
             if board[r][c] != word[i]:
                 return False
+            
             
             if (r,c) in visited:
                 return False
             
-            visited.add( (r,c) )
+            moves = [[1,0], [-1,0], [0,1],[0,-1]]
             
-            result = dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c-1,i+1) or dfs(r,c+1,i+1)
-            visited.remove( (r,c) )
+            visited.add((r,c))
+            result = False
+            for addR, addC in moves:
+                result = result or dfs(r+addR,c+addC, i+1)
+            
+            visited.remove( (r,c))
             
             return result
+            
         
-        for r in range( lenRow):
-            for c in range(lenCol):
-                if board[r][c] == word[0]:
-                    if dfs(r,c,0):
-                        return True
+        for i in range(lenR):
+            for j in range(lenC):
+                if dfs(i,j,0):
+                    return True
         
         return False
