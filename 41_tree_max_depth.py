6ee42601793f4ad4bc66e585937faa2161bf586d
inu <vr1090@gmail.com>
2022-05-08 18:59:43 +0700

    tree max depth

---
+++ 41_tree_max_depth.py
@@ -0,0 +1,19 @@
+"""
+max of 1 + max( left or right)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        elif not root.left and not root.right:
+            return 1
+        else:
+            return 1 + max( self.maxDepth(root.left), self.maxDepth(root.right))
+        
